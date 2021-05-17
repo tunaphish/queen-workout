8c8ad7c6056663c4003577300f1d6bcfe1f7caf9
Tuan Pham <tunaphishy37@gmail.com>
2021-05-16 18:06:04 -0700

    add some basic buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import 'antd/dist/antd.css';
-import { Layout } from 'antd';
+import './index.css';
+import { Layout, Button, Space } from 'antd';
 
 const { Header, Content } = Layout;
 
 const App = () => {
   return (
     <div >
-    <Layout>
-      <Header>Hi I am Tuanna man</Header>
-      <Content>Hi I am body</Content>
+    <Layout className="layout">
+      <Header className="header-title">QUEEN WORKOUT</Header>
+      <Content>
+        <div className="site-layout-content">
+          <Space direction="vertical" align="center">
+            <Button type="primary">Booty Workout Day 1</Button>
+            <Button type="primary" disabled>Booty Workout Day 2</Button>
+          </Space>
+        </div>
+      </Content>
     </Layout>
     </div>
   );
