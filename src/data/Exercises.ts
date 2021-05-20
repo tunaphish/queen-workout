8db31fbf64282977b7d048e3e7aff50d2d4d0512
Tuan Pham  🐟 <tuapha@amazon.com>
2021-05-20 13:23:23 -0700

    refactor data

--- src/data/Exercises.ts
+++ src/data/Exercises.ts
@@ -1,8 +1,10 @@
-import Exercise from '../../types/Exercise';
+import Exercise from '../types/Exercise';
 
 const HipThrust: Exercise = {
     name: "Hip Thrust",
     video: "https://www.instagram.com/p/BW6AKtYgjtS/"   
 }
 
-export default HipThrust;
+export {
+    HipThrust
+};
---
+++ src/data/WorkoutList.ts
@@ -0,0 +1,19 @@
+import Workout from '../types/Workout';
+import Round from '../types/Round';
+
+import { HipThrust } from './Exercises';
+
+const roundOne: Round = {
+    exercise: HipThrust,
+    sets: [30,30,30,30],
+}
+
+const WorkoutList: Workout[] = [
+    {
+        name: "Booty Day 1",
+        roundList: [roundOne],
+    }
+]
+
+
+export default WorkoutList;
