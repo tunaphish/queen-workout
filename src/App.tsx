a93726f672057a85cdfbe8f730dde8c46f6bfd06
Tuan Pham  🐟 <tuapha@amazon.com>
2021-05-21 16:19:34 -0700

    add css for new header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import './index.css';
+import styled from 'styled-components';
 import Home from './components/Home';
+
+const Header = styled.header`
+  background-color: red;
+  min-height: 64px;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const HomeLink = styled.a`
+  color: white;
+  margin: 10px;
+`;
+
+const RegularLink = styled.a`
+  color: white;
+  margin: 10px;
+`;
 
 const App = () => (
   <div>
-    <header className="header-title">QUEEN WORKOUT</header>
-    <div className="site-layout-content">
+    <Header>
+      <HomeLink>QUEEN WORKOUT</HomeLink>
+      <RegularLink>Test Link</RegularLink>
+    </Header>
+    <div>
       <Home />
     </div>
   </div>
