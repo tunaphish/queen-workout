01fcad694d38c6503040b93422c1d576ad5d8f0f
Tuan Pham  🐟 <tuapha@amazon.com>
2021-05-24 11:34:32 -0700

    add new exercises

--- src/data/Exercises.ts
+++ src/data/Exercises.ts
@@ -5,12 +5,48 @@ const HipThrust: Exercise = {
   video: 'https://www.instagram.com/p/BW6AKtYgjtS/',
 };
 
-const Temp: Exercise = {
-  name: 'Temp',
-  video: 'https://www.instagram.com/p/BW6AKtYgjtS/',
+const FrogPumps: Exercise = {
+  name: 'Frog Pumps',
+  video: 'https://www.instagram.com/p/BW6EN7WAbqI/',
+};
+
+const SquatWithCalfRaise: Exercise = {
+  name: 'Squat with Calf Raise',
+  video: 'https://www.instagram.com/p/BW6IB6OgUAb/',
+};
+
+const KneeUpStepBackLunges: Exercise = {
+  name: 'Knee Up Step Back Lunges',
+  video: 'https://www.instagram.com/p/BW6XhA-guKR/',
+};
+
+const StaticGluteBridgeActivation: Exercise = {
+  name: 'Static Glute Bridge Activation',
+  video: 'https://www.instagram.com/p/BiIsLtuA7_t/',
+};
+
+const DbAroundTheWorldLunges : Exercise = {
+  name: 'Db Around the World Lunges ',
+  video: 'https://www.instagram.com/p/BW3qPxMAV1E/',
+};
+
+const BandedHipAbductors : Exercise = {
+  name: 'Banded Hip Abductors',
+  video: 'https://www.instagram.com/p/BVgVCeFny7x/',
+};
+
+const CalfRaises : Exercise = {
+  name: 'Calf Raises ',
+  video: 'https://www.instagram.com/p/BW6VHkgAprB/',
 };
 
 export {
   HipThrust,
-  Temp,
+  FrogPumps,
+  SquatWithCalfRaise,
+  KneeUpStepBackLunges,
+  StaticGluteBridgeActivation,
+  DbAroundTheWorldLunges,
+  BandedHipAbductors,
+  CalfRaises,
 };
